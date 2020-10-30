fef007f24a89b269ba53c1c5c09409feddb4dd4b
martosfre <mtoscano@matoosfe.com>
2020-10-29 23:21:33 -0500

    Filter and order a number list in both asc and desc

--- src/main/java/com/matoosfe/example/java8/streams/FilterExample.java
+++ src/main/java/com/matoosfe/example/java8/streams/FilterExample.java
@@ -3,10 +3,13 @@
  */
 package com.matoosfe.example.java8.streams;
 
-import java.util.List;
 import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparingInt;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 import com.matoosfe.example.java8.Book;
 
@@ -30,8 +33,7 @@ public class FilterExample {
 	 * @return
 	 */
 	public static List<Book> filterBooksByYear(List<Book> books, Integer year) {
-		return books.stream().filter(b -> b.getBooPublicationYear() > year)
-				.collect(toList());
+		return books.stream().filter(b -> b.getBooPublicationYear() > year).collect(toList());
 	}
 
 	/**
@@ -44,8 +46,7 @@ public class FilterExample {
 	 * @return
 	 */
 	public static List<Book> filterBooksByYearUnique(List<Book> books, Integer year) {
-		return books.stream().filter(b -> b.getBooPublicationYear() > year)
-				.distinct().collect(toList());
+		return books.stream().filter(b -> b.getBooPublicationYear() > year).distinct().collect(toList());
 	}
 
 	/**
@@ -57,20 +58,46 @@ public class FilterExample {
 	 * @return
 	 */
 	public static List<Book> filterLimitBookByYear(List<Book> books, Integer year, Integer numberOfBooks) {
-		return books.stream().filter(b -> b.getBooPublicationYear() < year)
-				.limit(numberOfBooks).collect(toList());
+		return books.stream().filter(b -> b.getBooPublicationYear() < year).limit(numberOfBooks).collect(toList());
 	}
 
 	/**
 	 * Method to filter the books by year and skip a number of elements.
+	 * 
 	 * @param books
 	 * @param year
 	 * @param skipNumber
 	 * @return
 	 */
 	public static List<Book> filterBooksByYear(List<Book> books, Integer year, int skipNumber) {
-		return books.stream().filter(b -> b.getBooPublicationYear() > 2002)
-				.skip(skipNumber).collect(toList());
+		return books.stream().filter(b -> b.getBooPublicationYear() > 2002).skip(skipNumber).collect(toList());
+	}
+
+	/**
+	 * Method to filter the odd numbers without duplicates and print them from a
+	 * list of numbers.
+	 * 
+	 * @param numbers
+	 */
+	public static void filterImparNumberWithoutDuplicates(List<Integer> numbers) {
+		numbers.stream().filter(n -> n % 2 != 0).distinct().forEach(System.out::println);
+	}
+
+	/**
+	 * Method to filter the odd numbers without duplicates, order and print them
+	 * from a list of numbers
+	 * 
+	 * @param numbers
+	 * @param des
+	 */
+	public static void filterImparNumberOrderWithoutDuplicates(List<Integer> numbers, boolean des) {
+		if (des) {
+			numbers.stream().filter(n -> n % 2 != 0).distinct().sorted(Comparator.reverseOrder())
+					.forEach(System.out::println);
+		} else {
+			numbers.stream().filter(n -> n % 2 != 0).distinct().sorted(comparingInt(n -> n))
+					.forEach(System.out::println);
+		}
 	}
 
 	/**
@@ -97,9 +124,21 @@ public class FilterExample {
 
 		System.out.println("\n\nFilter Books by Year less than:" + 2006 + " and choose the first two books");
 		FilterExample.filterLimitBookByYear(books, 2006, 2).forEach(System.out::println);
-		
+
 		System.out.println("\n\nFilter the Book by Year older than:" + 2002 + " and skip the first two books");
 		FilterExample.filterBooksByYear(books, 2002, 2).forEach(System.out::println);
+
+		System.out.println("\n\nFilter the odd numbers without duplicates and print from a number list ");
+		List<Integer> numberList = Arrays.asList(7, 8, 9, 10, 11, 12, 13, 15, 15, 16, 17, 1, 2, 3, 4, 5, 6, 19, 19, 20);
+		FilterExample.filterImparNumberWithoutDuplicates(numberList);
+
+		System.out.println(
+				"\n\nFilter the odd numbers without duplicates, order descendent and print from a number list ");
+		FilterExample.filterImparNumberOrderWithoutDuplicates(numberList, true);
+
+		System.out.println(
+				"\n\nFilter the odd numbers without duplicates, order ascendent and print from a number list ");
+		FilterExample.filterImparNumberOrderWithoutDuplicates(numberList, false);
 
 	}
 
